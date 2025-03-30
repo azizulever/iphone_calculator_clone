730ccef6e92c72814183621034bcc978bc0e10ba
Azizul Islam <contact.azizulislam@gmail.com>
2025-03-30 12:59:57 +0600

    add buttons

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -17,16 +17,71 @@ class _IphoneCalculatorCloneState extends State<IphoneCalculatorClone> {
       home: Scaffold(backgroundColor: Colors.black,
         body: Column(
           children: [
-            TextField(
-              controller: _textEditingController,style: TextStyle(color: Colors.white),
-              textAlign: TextAlign.end,
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(16),
-                enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextField(
+                controller: _textEditingController,style: TextStyle(color: Colors.white, fontSize: 48),
+                textAlign: TextAlign.end,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(16),
+                  enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+                  focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+                ),
               ),
             ),
-            Row(),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(onPressed: () {}, child: Text('AC')),
+                    TextButton(onPressed: () {}, child: Text('+/-')),
+                    TextButton(onPressed: () {}, child: Text('%')),
+                    TextButton(onPressed: () {}, child: Text('÷')),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(onPressed: () {}, child: Text('7')),
+                    TextButton(onPressed: () {}, child: Text('8')),
+                    TextButton(onPressed: () {}, child: Text('9')),
+                    TextButton(onPressed: () {}, child: Text('×')),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(onPressed: () {}, child: Text('4')),
+                    TextButton(onPressed: () {}, child: Text('5')),
+                    TextButton(onPressed: () {}, child: Text('6')),
+                    TextButton(onPressed: () {}, child: Text('-')),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(onPressed: () {}, child: Text('1')),
+                    TextButton(onPressed: () {}, child: Text('2')),
+                    TextButton(onPressed: () {}, child: Text('3')),
+                    TextButton(onPressed: () {}, child: Text('+')),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(onPressed: () {}, child: Text('0')),
+                    TextButton(onPressed: () {}, child: Text(' ')),
+                    TextButton(onPressed: () {}, child: Text('.')),
+                    TextButton(onPressed: () {}, child: Text('=')),
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
